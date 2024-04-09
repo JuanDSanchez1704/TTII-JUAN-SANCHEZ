59afd68482288093a305674eb2d56987b8026def
Juan Sanchez <juan.sanchez.garcia@correounivalle.edu.co>
2024-04-08 20:22:52 -0500

    se agrega cambio para ver en el remoto

--- logica.js
+++ logica.js
@@ -1,2 +1,4 @@
 alert("Bienvenidos a taller tecnologico II")
 console.log("esto que estoy escribiendo se vera en la concola de logica:js:2 HTML")
+console.log("esta va ser la tercera version")
+console.log("este cambio ira hasta remoto")
